cb29edd86e5c23351e5f332dcb2fe8ba040d5a30
XC3000 <richardrozario.rr@gmail.com>
2022-03-11 01:09:56 +0530

    inital commit

---
+++ src/components/layout/Content.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Sidebar} from "./Sidebar";
+
+const Content = () => {
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  );
+};
+
+export default Content;
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = firebase.initializeApp({
+  apiKey: "AIzaSyCeMVTiRp1uSTOVSFhzuXNE47jnE7X6gyk",
+  authDomain: "todoist-tut-9a214.firebaseapp.com",
+  databaseURL:
+    "https://todoist-tut-9a214-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "todoist-tut-9a214",
+  storageBucket: "todoist-tut-9a214.appspot.com",
+  messagingSenderId: "571265843576",
+  appId: "1:571265843576:web:70566844c065e3a35bc88d",
+});
+
+export { firebaseConfig as firebase };
